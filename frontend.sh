437be56b1eac4964f76f529f4b4d5c1cb7d47e9c
Harshit M <harshitmittal27@gmail.com>
2022-12-09 16:52:43 +0530

    Making the Code DRY

--- frontend.sh
+++ frontend.sh
@@ -9,11 +9,17 @@ STAT $?
 APP_LOC=/usr/share/nginx/html
 
 DOWNLOAD_APP_CODE
-exit
 
 mv frontend-main/static/* .
+
+PRINT "Copy RoboShop Configuration File"
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $?
 
+PRINT "Enable Nginx Service"
 systemctl enable nginx
-systemctl restart nginx
+STAT $?
 
+PRINT "Start Nginx Service"
+systemctl restart nginx
+STAT $?
