8d08a660a120bc7c167c875bada4e3ca49a6372b
Harshit M <harshitmittal27@gmail.com>
2022-12-11 05:22:32 +0530

    Payment script

--- common.sh
+++ common.sh
@@ -129,4 +129,12 @@ PYTHON() {
   PRINT "Install Python Dependencies"
   pip3 install -r requirements.txt &>>$LOG
   STAT $?
+
+  USER_ID=$(id -u roboshop)
+  GROUP_ID=$(id -g roboshop)
+  sed -i -e "/uid/ c uid = ${USER_ID}" -e "/uid/ c uid = ${GROUP_ID}" ${COMPONENT}.ini
+
+  SYSTEMD_SETUP
 }
+
+
