5285651c715b87b0b1000cdce0615c415ed8cb3d
Harshit M <harshitmittal27@gmail.com>
2022-12-14 07:34:33 +0530

    running script

--- common.sh
+++ common.sh
@@ -43,7 +43,7 @@ STAT $?
 
 SYSTEMD_SETUP() {
   PRINT "Configure endpints for SystemD Configuration"
-  sed -i -e 's/MONGO_DNSNAME/dev-mongodb.devopsb69.online/' -e 's/REDIS_ENDPOINT/dev-redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/dev-catalogue.devopsb69.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
+  sed -i -e 's/MONGO_DNSNAME/dev-mongodb.devopsb69.online/' -e 's/REDIS_ENDPOINT/dev-redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/dev-catalogue.devopsb69.online/' -e 's/MONGO_ENDPOINT/dev-mongodb.devopsb69.online/' -e 's/REDIS_HOST/dev-redis.devopsb69.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
   STAT $?
 
   PRINT "Setup SystemD Service"
